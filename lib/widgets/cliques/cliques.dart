2aef2174b6167804d8340e58b8439f209c55328c
Don Boots <don@donboots.com>
2018-10-07 19:08:07 -0400

    optimize scrolling list

--- lib/widgets/cliques/cliques.dart
+++ lib/widgets/cliques/cliques.dart
@@ -12,30 +12,15 @@ class CliquesPage extends StatefulWidget {
 }
 
 class _CliquesPageState extends State<CliquesPage> {
+  String _selected = 'CLIQUES';
   PageController _controller = PageController();
+  var _data = [];
 
   @override
   void initState() {
     super.initState();
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    return _getBody();
-  }
-
-  _getBody() {
-    var data = [
-      {
-        "_id": "5a669b9e06bb8c0011119665",
-        "updatedAt": "2018-01-23T02:19:10.274Z",
-        "createdAt": "2018-01-23T02:19:10.274Z",
-        "owner": "5a654cfc29c5ed0010435a4c",
-        "__v": 0,
-        "members": ["5a654cfc29c5ed0010435a4c"],
-        "name": "WOO"
-      }
-    ];
+    var data = [];
 
     for (num i = 0; i < 125; i++) {
       data.add({
@@ -49,18 +34,20 @@ class _CliquesPageState extends State<CliquesPage> {
       });
     }
 
-    List<Row> cliques = data
-        .map((d) => Row(children: <Widget>[
-              Container(
-                  margin: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
-                  child: CircleAvatar(
-                      radius: 15.0,
-                      backgroundColor: Color(0xFFFF0000),
-                      child: Text('A', style: TextStyle(color: Colors.white)))),
-              Text(d["name"],
-                  style: TextStyle(fontSize: 16.0, color: Color(0xFF666666)))
-            ]))
-        .toList();
+    setState(() {
+      _data = data;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _getBody();
+  }
+
+  _getBody() {
+    BoxDecoration decoration = BoxDecoration(
+        border:
+            Border(bottom: BorderSide(width: 2.0, color: Color(0xFF666666))));
 
     return Scaffold(
         body: Container(
@@ -73,46 +60,95 @@ class _CliquesPageState extends State<CliquesPage> {
                 child: Container(
                     padding: EdgeInsets.only(bottom: 10.0),
                     margin: EdgeInsets.only(left: 20.0, right: 20.0),
-                    decoration: BoxDecoration(
-                        border: Border(
-                            bottom: BorderSide(
-                                width: 2.0, color: Color(0xFF666666)))),
+                    decoration: (_selected == 'CLIQUES') ? decoration : null,
                     child: Utils.getTitle('CLIQUES', () {
-                      _controller.animateToPage(0,
-                          duration: new Duration(milliseconds: 200),
-                          curve: Curves.easeIn);
+                      setState(() {
+                        _selected = 'CLIQUES';
+                      });
+                      _controller.jumpToPage(0);
                     }))),
             Expanded(
                 child: Container(
                     padding: EdgeInsets.only(bottom: 10.0),
                     margin: EdgeInsets.only(left: 20.0, right: 20.0),
-                    decoration: BoxDecoration(
-                        border: Border(
-                            bottom: BorderSide(
-                                width: 2.0, color: Color(0xFF666666)))),
+                    decoration: (_selected == 'FRIENDS') ? decoration : null,
                     child: Utils.getTitle('FRIENDS', () {
-                      _controller.animateToPage(1,
-                          duration: new Duration(milliseconds: 200),
+                      setState(() {
+                        _selected = 'FRIENDS';
+                      });
+                      _controller.jumpToPage(1);
+                      /*_controller.animateToPage(1,
+                          duration: new Duration(milliseconds: 250),
                           curve: Curves.easeIn);
+													*/
                     })))
           ])),
       Expanded(
-        child: PageView(controller: _controller, children: <Widget>[
-          Column(children: <Widget>[
-            Expanded(
-                child: SingleChildScrollView(child: Column(children: cliques))),
-            BottomNav().build(
-                leftIcon: FontAwesomeIcons.arrowLeft,
-                leftAction: () { Navigator.pop(context); },
-                middleAction: () {},
-                middleLabel: 'CREATE')
-          ]),
-          Column(children: <Widget>[
-            Expanded(
-                child: SingleChildScrollView(child: Column(children: cliques))),
-            BottomNav().build(middleAction: () {}, middleLabel: 'ADD FRIEND')
-          ])
-        ]),
+        child: PageView(
+            onPageChanged: (page) {
+              var selected = 'CLIQUES';
+
+              if (page == 1) {
+                selected = 'FRIENDS';
+              }
+
+              setState(() {
+                _selected = selected;
+              });
+            },
+            controller: _controller,
+            children: <Widget>[
+              Column(children: <Widget>[
+                Expanded(
+                    child: ListView.builder(
+                  itemCount: _data.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                          radius: 15.0,
+                          backgroundColor: Color(0xFFFF0000),
+                          child:
+                              Text('A', style: TextStyle(color: Colors.white))),
+                      title: Text(_data[index]['name'],
+                          style: TextStyle(
+                              fontSize: 16.0, color: Color(0xFF666666))),
+                    );
+                  },
+                )),
+                BottomNav().build(
+                    leftIcon: FontAwesomeIcons.arrowLeft,
+                    leftAction: () {
+                      Navigator.pop(context);
+                    },
+                    middleAction: () {},
+                    middleLabel: 'CREATE')
+              ]),
+              Column(children: <Widget>[
+                Expanded(
+                    child: ListView.builder(
+                  itemCount: _data.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                          radius: 15.0,
+                          backgroundColor: Color(0xFFFF0000),
+                          child:
+                              Text('A', style: TextStyle(color: Colors.white))),
+                      title: Text(_data[index]['name'],
+                          style: TextStyle(
+                              fontSize: 16.0, color: Color(0xFF666666))),
+                    );
+                  },
+                )),
+                BottomNav().build(
+                    leftIcon: FontAwesomeIcons.arrowLeft,
+                    leftAction: () {
+											Navigator.pop(context);
+										},
+                    middleAction: () {},
+                    middleLabel: 'ADD FRIEND')
+              ])
+            ]),
       )
     ]))));
   }
