b56b3951aa08bcf3444a3e16e952e2049782040a
Don Boots <don@donboots.com>
2018-10-21 19:04:22 -0400

    remove dev url

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -3,8 +3,8 @@ import 'package:Social/services/shared-prefs.dart';
 
 class API extends Object {
   // TODO: Use proper url based on dev/prod settings
-  // final String url = 'https://sleepy-tor-82012.herokuapp.com/v1';
-  final String url = 'http://0.0.0.0:3000/v1';
+  final String url = 'https://sleepy-tor-82012.herokuapp.com/v1';
+  //final String url = 'http://0.0.0.0:3000/v1';
   final Client httpClient = Client();
 	String get token => _getFoo();
 
