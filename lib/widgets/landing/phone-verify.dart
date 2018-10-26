f74ac12078c4e7d896d76668694c801ee8919226
polaris327 <webstar317@yahoo.com>
2018-10-25 22:36:44 -0400

    Update event to focus automatically in phone-verify

--- lib/widgets/landing/phone-verify.dart
+++ lib/widgets/landing/phone-verify.dart
@@ -8,9 +8,54 @@ class PhoneVerifyPage extends StatefulWidget {
 }
 
 class _PhoneVerifyPageState extends State<PhoneVerifyPage> {
+  final FocusNode _firstFocus = FocusNode();
+  final FocusNode _secondFocus = FocusNode();
+  final FocusNode _thirdFocus = FocusNode();
+  final FocusNode _forthFocus = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return _getBody(context);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  _fieldFocusChange(context, currentFocus, nextFocus) {
+    currentFocus.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);
+  }
+
+  _firstChange(text, context) {
+    if (text.length == 1) {
+      _fieldFocusChange(context, _firstFocus, _secondFocus);
+    }
+  }
+  _secondChange(text, context) {
+    if (text.length == 1) {
+      _fieldFocusChange(context, _secondFocus, _thirdFocus);
+    } else if (text.length == 0) {
+      _fieldFocusChange(context, _secondFocus, _firstFocus);
+    }
+  }
+  _thirdChange(text, context) {
+    if (text.length == 1) {
+      _fieldFocusChange(context, _thirdFocus, _forthFocus);
+    } else if (text.length == 0) {
+      _fieldFocusChange(context, _thirdFocus, _secondFocus);
+    }
+  }
+  _forthChange(text, context) {
+    if (text.length == 0) {
+      _fieldFocusChange(context, _forthFocus, _thirdFocus);
+    }
   }
 
   _getBody(context) {
@@ -43,7 +88,7 @@ class _PhoneVerifyPageState extends State<PhoneVerifyPage> {
                   Expanded(
                   child: Container(
                     padding: EdgeInsets.all(5.0),
-                    child: TextFormField(
+                    child: TextField(
                       maxLength: 1,
                       decoration: const InputDecoration(
                         labelText: '',
@@ -52,6 +97,14 @@ class _PhoneVerifyPageState extends State<PhoneVerifyPage> {
                         )
                       ),
                       keyboardType: TextInputType.number,
+                      onChanged: (text) {
+                        _firstChange(text, context);
+                      },
+                      textInputAction: TextInputAction.next,
+                      focusNode: _firstFocus,
+                      onSubmitted: (term) {
+                        _fieldFocusChange(context, _firstFocus, _secondFocus);
+                      },
                       style: TextStyle(
                           fontFamily: 'Lato',
                           fontSize: 16.0,
@@ -62,7 +115,7 @@ class _PhoneVerifyPageState extends State<PhoneVerifyPage> {
                   Expanded(
                   child: Container(
                     padding: EdgeInsets.all(5.0),
-                    child: TextFormField(
+                    child: TextField(
                       maxLength: 1,
                       decoration: const InputDecoration(
                         labelText: '',
@@ -71,6 +124,14 @@ class _PhoneVerifyPageState extends State<PhoneVerifyPage> {
                         )
                       ),
                       keyboardType: TextInputType.number,
+                      onChanged: (text) {
+                        _secondChange(text, context);
+                      },
+                      textInputAction: TextInputAction.next,
+                      focusNode: _secondFocus,
+                      onSubmitted: (term) {
+                        _fieldFocusChange(context, _secondFocus, _thirdFocus);
+                      },
                       style: TextStyle(
                           fontFamily: 'Lato',
                           fontSize: 16.0,
@@ -81,7 +142,7 @@ class _PhoneVerifyPageState extends State<PhoneVerifyPage> {
                   Expanded(
                   child: Container(
                     padding: EdgeInsets.all(5.0),
-                    child: TextFormField(
+                    child: TextField(
                       maxLength: 1,
                       decoration: const InputDecoration(
                         labelText: '',
@@ -90,6 +151,14 @@ class _PhoneVerifyPageState extends State<PhoneVerifyPage> {
                         )
                       ),
                       keyboardType: TextInputType.number,
+                      onChanged: (text) {
+                        _thirdChange(text, context);
+                      },
+                      textInputAction: TextInputAction.next,
+                      focusNode: _thirdFocus,
+                      onSubmitted: (term) {
+                        _fieldFocusChange(context, _thirdFocus, _forthFocus);
+                      },
                       style: TextStyle(
                           fontFamily: 'Lato',
                           fontSize: 16.0,
@@ -100,7 +169,7 @@ class _PhoneVerifyPageState extends State<PhoneVerifyPage> {
                   Expanded(
                   child: Container(
                     padding: EdgeInsets.all(5.0),
-                    child: TextFormField(
+                    child: TextField(
                       maxLength: 1,
                       decoration: const InputDecoration(
                         labelText: '',
@@ -109,6 +178,15 @@ class _PhoneVerifyPageState extends State<PhoneVerifyPage> {
                         )
                       ),
                       keyboardType: TextInputType.number,
+                      onChanged: (text) {
+                        _forthChange(text, context);
+                      },
+                      textInputAction: TextInputAction.done,
+                      focusNode: _forthFocus,
+                      onSubmitted: (term) {
+                        _forthFocus.unfocus();
+                        Navigator.pushNamed(context, 'dashboard');
+                      },
                       style: TextStyle(
                           fontFamily: 'Lato',
                           fontSize: 16.0,
