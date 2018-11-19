68d034e90dd12a7ea160c014da7fb709a1397a4d
Don Boots <don@donboots.com>
2018-11-19 01:49:57 -0500

    project updates

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart';
 
 class API extends Object {
-  //final String url = 'https://sleepy-tor-82012.herokuapp.com/v1';
-  final String url = 'http://0.0.0.0:3000/v1';
+  final String url = 'https://sleepy-tor-82012.herokuapp.com/v1';
+  //final String url = 'http://0.0.0.0:3000/v1';
   final Client httpClient = Client();
 }
